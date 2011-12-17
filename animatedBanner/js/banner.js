f11c258304de41636e6324bb811280d2833c5285
Aayush Ahuja <aayush.ahuja1991@gmail.com>
2011-12-17 19:49:41 +0530

    second commit

--- animatedBanner/js/banner.js
+++ animatedBanner/js/banner.js
@@ -1,7 +1,10 @@
+	//no of circles
 	var number=35;	
 	var widthBanner = 1000;
 	var heightBanner = 100;
+	//Raphael Object
 	var paper= Raphael('banner',0,widthBanner, 800, 200);
+	//Styles for circles and line joining their center
 	var filler = {
 		fill:'gray',
 		opacity:'0.1'
@@ -9,9 +12,9 @@
 	var filler2 = {
 		fill:'black',
 		opacity:'0.3'
-		};	
-	//location and then size
-//lines
+		};
+			
+	//2-D array containing the line objects between any 2 circles
 	var lines = new Array(number);
 	for (var i = 0; i < number; i++) {
     	lines[i] = new Array(number);
@@ -19,7 +22,7 @@
 			lines[i][j] =null;
 		}
 	  }
-
+	//circle object
 	var x,y,velX,velY,colour;
 	function ball(a,b,c,d,e){
 		this.x=a;
@@ -29,9 +32,10 @@
 		this.colour=e;
 		}
 	var a=false;
+	
+	//initialising objects
 	var arrShape=new Array();
 	var arrPos=new Array();
-
 	var colours=new Array();
 	colours[0]='yellow';
 	colours[1]='black';	
@@ -62,6 +66,7 @@
 		arrShape[i].node.setAttribute('class', 'colour4');}		
 		}
 		
+		//mouse functionality
 		$(".colour1").mouseover(function(){
 			
 			for(var i=0;i<number;i++){
@@ -98,68 +103,44 @@
 			}
 			
 			});			
-		/*
-	for(var i=0;i<number;i++){
-		arrShape[i].node.id="circle"+number;
-		}
 		
-//	arrShape.nodes.id="circle";
-
-	var a=false;
-	for(var i=0;i<number;i++){
-	$("#circle"+number).mouseenter(function(e){
-		a=true;
-		clearTimeout(time);
-		arrPos[number].x=e.pageX;
-		arrPos[number].y=e.pageY;
-		arrShape[number].attr("cx", x); // set new opacity attribute
-		arrShape[number].attr("cy", y);
-	});	
-	$("#circle"+number).mouseleave(function(e){
-	//	i=e.pageX-1;
-		//j=e.pageY-1;
-		a=false;
-//		speed=speed-1;
-		clearTimeout(time);
-		animate();
-		});
-*/
-	
+	//delay
 	var delay=50;	
 	var time ;
+	
 	function distance(x1,y1,x2,y2){
 		return Math.sqrt(((x2-x1)*(x2-x1))+((y2-y1)*(y2-y1)));
 		}
+		
 	var closeness=50;	
 	var ij=0;
+	
+	//main animation function that calls itself
 	function animate(){
 		
 		for(var i=0;i<number;i++){	
 			var a=arrPos[i].x;
 			var b=arrPos[i].y;
-			
+			//bouncing condition
 			if(arrPos[i].x<=0 || arrPos[i].x>=widthBanner){arrPos[i].velX=(-1*arrPos[i].velX);}
 			if(arrPos[i].y<=0 || arrPos[i].y>=heightBanner){arrPos[i].velY=(-1*arrPos[i].velY);}
+			
+			//line joining the center code
 			if(ij%40==0){
 			for(var ii=0;ii<number;ii++){
 				for(var j=ii+1;j<number;j++){
 					var c=arrPos[j].x;
 					var d=arrPos[j].y;
 					if((Math.abs(arrPos[ii].x - arrPos[j].x) <= closeness) && (Math.abs(arrPos[ii].y - arrPos[j].y) <= closeness)) {
-//					     var line = Line(arrPos[i].x,arrPos[i].y,arrPos[j].x ,arrPos[j].y, paper);
 						if(lines[ii][j]!=null){
-//								Line(arrPos[ii].x,arrPos[ii].y,arrPos[j].x ,arrPos[j].y,paper).redraw;	
 								lines[ii][j].attr("path","M" + arrPos[ii].x + " " + arrPos[ii].y + " L" + arrPos[j].x + " " + arrPos[j].y);	
 								lines[ii][j].attr(filler2);
 							}
 							
 						else{
 							lines[ii][j] =paper.path("M" + arrPos[ii].x + " " + arrPos[ii].y + " L" + arrPos[j].x + " " + arrPos[j].y);	
-							//lines[ii][j] =paper.path("M" + 0 + " " + 0 + " L" + 0 + " " + 0);	
 							lines[ii][j].attr(filler2);
 							}
-							
-						
 					}
 					else{
 					if(lines[ii][j]!=null){
@@ -169,24 +150,22 @@
 				}
 			}}
 			ij++;
-			//x=x+1;
+			//updating positions
 			arrPos[i].x=arrPos[i].x+arrPos[i].velX;
-			//y=y+1;
 			arrPos[i].y=arrPos[i].y+arrPos[i].velY;
-			//checking bundary
-		    //c.attr("cx", x); // set new opacity attribute
-			//drawing part
-			arrShape[i].attr("cx",a);
 			
-			//c.attr("cy", y);
-			arrShape[i].attr("cy",b);			
+			arrShape[i].attr("cx",arrPos[i].x);
+			arrShape[i].attr("cy",arrPos[i].y);			
 //			arrShape[i].glow({width:1.3,opacity:.1});			
 			
 		}
+		
+		
 		time=setTimeout("animate()", delay);
 	}
 	
+	//entry point
 	$(document).ready(function(){
 		animate();	
 	});
-	//}
+	

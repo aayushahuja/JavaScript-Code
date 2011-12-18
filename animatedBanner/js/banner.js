a2a97c4131b37f0bd0f6cb10e03edaef440090c9
Aayush Ahuja <aayush.ahuja1991@gmail.com>
2011-12-18 15:09:05 +0530

    Update animatedBanner/js/banner.js

--- animatedBanner/js/banner.js
+++ animatedBanner/js/banner.js
@@ -1,5 +1,5 @@
 //number of circles
-	var number=35;	
+	var number=30;	
 	var widthBanner = 1000;
 	var heightBanner = 100;
 //Raphael object	
@@ -7,11 +7,13 @@
 //Styles for circles and the line joining them	
 	var filler = {
 		fill:'gray',
-		opacity:'0.1'
+		opacity:'0.1',
+		stroke : 'black',
+		'stroke-width' : 3
 		};
 	var filler2 = {
 		fill:'black',
-		opacity:'0.4'
+		opacity:'0.4',
 		};	
 //2D array for lines between any two pair(inefficient now as less than half are used)
 	var lines = new Array(number);
@@ -40,10 +42,10 @@
 	var colour3 = new Array();	
 	var colour4 = new Array();
 	var colours=new Array();
-	colours[0]='yellow';
-	colours[1]='black';	
-	colours[2]='green';	
-	colours[3]='blue';	
+	colours[0]='green';
+	colours[1]='blue';	
+	colours[2]='gray';	
+	colours[3]='yellow';	
 	var iniX,iniY,iniAng,radius,rad;
 	var temp1=0;
 	for(var i=0;i<number;i++){
@@ -76,64 +78,74 @@
 		colour4.push(i);numColour4++;
 		}		
 		}
-		var mouseOverOpacity=.3;
+		var mouseOverOpacity=.4;
 		var time;
+		var text1=new Array();
+		var text2=new Array();
+		var text3=new Array();
+		var text4=new Array();						
 	//MOUSE Functionality	
 		$(".colour1").mouseover(function(e){
 			for(var i=0;i<numColour1;i++){
-				if(colour1[i]!=""){
+					text1[i]= paper.text(arrPos[colour1[i]].x,arrPos[colour1[i]].y,'Astronomy\nClub');
+					//text1[i].attr("href","http://technocracy.ideas2ignite.com/astronomy/");
+					//text1[i].attr("cursor","pointer");
 					arrShape[colour1[i]].attr("opacity",mouseOverOpacity);
 					arrShape[colour1[i]].attr("cx", arrPos[colour1[i]].x); 
 					arrShape[colour1[i]].attr("cy", arrPos[colour1[i]].y);
-					}
 			}
 		clearTimeout(time);	
 		});	
-		$(".colour1").mouseout(function(){
+		var time2;
+		$(".colour1").mouseleave(function(){
 			for(var i=0;i<numColour1;i++){
-				if(colour1[i]!=""){
+					text1[i].attr("text",'');
 					arrShape[colour1[i]].attr("opacity",.1);
-					}
+					
 			}			
-		animate();	
+		animate();
 		});
 		$(".colour2").mouseover(function(e){
 			for(var i=0;i<numColour2;i++){
-				if(colour2[i]!=""){
+					text2[i]= paper.text(arrPos[colour2[i]].x,arrPos[colour2[i]].y,'Electronics\nClub');
+					//text2[i].attr("href","http://www.technocracy-iitd.org/electronics/");
+					//text2[i].attr("cursor","pointer");
 					arrShape[colour2[i]].attr("opacity",mouseOverOpacity);
 					arrShape[colour2[i]].attr("cx", arrPos[colour2[i]].x); 
 					arrShape[colour2[i]].attr("cy", arrPos[colour2[i]].y);
-					}
+
 			}
 		clearTimeout(time);	
 		});	
 		$(".colour2").mouseout(function(){
 			for(var i=0;i<numColour2;i++){
-				if(colour2[i]!=""){
+					text2[i].attr("text",'');
 					arrShape[colour2[i]].attr("opacity",.1);
-					}
+					
 			}			
 		animate();	
 		});
 		$(".colour3").mouseover(function(e){
 			for(var i=0;i<numColour3;i++){
-				if(colour3[i]!=""){
+					text3[i]= paper.text(arrPos[colour3[i]].x,arrPos[colour3[i]].y,'Economics\nClub');
+					//text3[i].attr("href","http://www.facebook.com/ecoclubiitd");
+					//text3[i].attr("cursor","pointer");
 					arrShape[colour3[i]].attr("opacity",mouseOverOpacity);
 					arrShape[colour3[i]].attr("cx", arrPos[colour3[i]].x); 
 					arrShape[colour3[i]].attr("cy", arrPos[colour3[i]].y);
 					}
-			}
+			
 		clearTimeout(time);	
 		});	
 		$(".colour3").mouseout(function(){
 			for(var i=0;i<numColour3;i++){
-				if(colour3[i]!=""){
+					text3[i].attr("text",'');
 					arrShape[colour3[i]].attr("opacity",.1);
-					}
+					
 			}			
 		animate();	
 		});
-				
+
 		$(".colour4").mouseover(function(e){
 			for(var i=0;i<numColour4;i++){
 				if(colour4[i]!=""){
@@ -146,13 +158,13 @@
 		});	
 		$(".colour4").mouseout(function(){
 			for(var i=0;i<numColour4;i++){
-				if(colour4[i]!=""){
+				//	text4[i].attr("text",'');
 					arrShape[colour4[i]].attr("opacity",.1);
-					}
+					
 			}			
 		animate();	
 		});
-				
+
 	//delay between animation frames
 	var delay=50;	
 	function distance(x1,y1,x2,y2){
@@ -161,39 +173,67 @@
 	var closeness=50;	
 	var ij=0;
 	//Main Animation function that calls itself
+	var numberOfConnectedLines=new Array();
+	for(var i=0;i<number;i++){
+		numberOfConnectedLines[i]=0;
+	}
 	function animate(){
+	//	clearTimeout(time2);
 		for(var i=0;i<number;i++){	
 			var a=arrPos[i].x;
 			var b=arrPos[i].y;
-			
+
 			//Bouncing off the walls condition
 			if(arrPos[i].x<=0 || arrPos[i].x>=widthBanner){arrPos[i].velX=(-1*arrPos[i].velX);}
 			if(arrPos[i].y<=0 || arrPos[i].y>=heightBanner){arrPos[i].velY=(-1*arrPos[i].velY);}
 			if(ij%40==0){
-			
+
 			//Lines between circles
 			for(var ii=0;ii<number;ii++){
 				for(var j=ii+1;j<number;j++){
 					var c=arrPos[j].x;
 					var d=arrPos[j].y;
 					if((Math.abs(arrPos[ii].x - arrPos[j].x) <= closeness) && (Math.abs(arrPos[ii].y - arrPos[j].y) <= closeness)) {
-
+						//console.log(numberOfConnectedLines[4]);
+						arrShape[ii].attr("opacity", 0.2);
+						arrShape[j].attr("opacity", 0.2);								
 						if(lines[ii][j]!=null){
+								if(lines[ii][j].attr("path")=="M0,0L0,0"){	
+							
+									numberOfConnectedLines[ii]=(numberOfConnectedLines[ii]+1);
+									numberOfConnectedLines[j]=(numberOfConnectedLines[j]+1);
+								}
 								lines[ii][j].attr("path","M" + arrPos[ii].x + " " + arrPos[ii].y + " L" + arrPos[j].x + " " + arrPos[j].y);	
 								lines[ii][j].attr(filler2);
 							}
-							
+
 						else{
+							numberOfConnectedLines[ii]=(numberOfConnectedLines[ii]+1);
+							numberOfConnectedLines[j]=(numberOfConnectedLines[j]+1);
 							lines[ii][j] =paper.path("M" + arrPos[ii].x + " " + arrPos[ii].y + " L" + arrPos[j].x + " " + arrPos[j].y);	
 							lines[ii][j].attr(filler2);
 							}
-							
-						
+
+
 					}
 					else{
-					if(lines[ii][j]!=null){
-								lines[ii][j].attr("path","M" + "0" + " " + "0" + " L" + "0" + " " + "0");	
+						if(lines[ii][j]!=null){
+							//if()
+							console.log(numberOfConnectedLines[ii]);
+						//	console.log(lines[ii][j].attr("path"));
+							if(lines[ii][j].attr("path")!="M0,0L0,0"){	
+							
+							numberOfConnectedLines[ii]=(numberOfConnectedLines[ii]-1);
+							numberOfConnectedLines[j]=(numberOfConnectedLines[j]-1);
 							}
+							
+							lines[ii][j].attr("path","M" + "0" + " " + "0" + " L" + "0" + " " + "0");	
+							if(numberOfConnectedLines[ii]<=0){
+								arrShape[ii].attr("opacity", 0.1);}
+							if(numberOfConnectedLines[j]<=0){								
+								arrShape[j].attr("opacity", 0.1);}																		
+							
+						}
 					}
 				}
 			}}
@@ -204,13 +244,12 @@
 			arrShape[i].attr("cx",arrPos[i].x);
 			arrShape[i].attr("cy",arrPos[i].y);			
 //			arrShape[i].glow({width:1.3,opacity:.1});			
-			
+
 		}
 //		clearTimeout(time)
 		time=setTimeout("animate()", delay);
 	}
-	
+
 	$(document).ready(function(){
 		animate();	
 	});
-	
